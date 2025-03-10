057be591f3647e9ac8de3660c6e7509ec064e70d
anhkhoa706 <anhkhoadeptrai706@gmail.com>
2025-03-10 19:18:59 +0800

    Remove cfg.freeze in ReIDSearcher

--- ReIDSearcher.py
+++ ReIDSearcher.py
@@ -31,7 +31,6 @@ class ReIDSearcher:
             cfg.merge_from_file(self.config_file)
         cfg.TEST.WEIGHT = self.weight_path
         cfg.TEST.RE_RANKING = self.re_ranking
-        cfg.freeze()
 
         output_dir = cfg.OUTPUT_DIR
         if output_dir and not os.path.exists(output_dir):
